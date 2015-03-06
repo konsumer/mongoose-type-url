6ad58f704f88180ae81985b53189f2f2ee2f86c6
Walter Franchetti <walter.franchetti@gmail.com>
2015-03-06 18:55:17 -0300

    return normalizeurl(val); to return normalizeUrl(val);

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 var normalizeUrl = require('normalizeurl');
 
-function Url (path, options) {
+function Url(path, options) {
 	mongoose.SchemaTypes.String.call(this, path, options);
-	function validateUrl (val) {
+
+	function validateUrl(val) {
 		var urlRegexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 		return urlRegexp.test(val);
 	}
@@ -13,7 +14,7 @@ function Url (path, options) {
 Url.prototype.__proto__ = mongoose.SchemaTypes.String.prototype;
 
 Url.prototype.cast = function (val) {
-	return normalizeurl(val);
+	return normalizeUrl(val);
 };
 
 mongoose.SchemaTypes.Url = module.exports = Url;
