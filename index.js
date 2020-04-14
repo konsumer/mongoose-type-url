3ba029b6e5a5f94c1c158ea933de5a8a66cf34a3
Matteo Di Sabatino <matteo.di.sabatino.1989@gmail.com>
2020-04-14 17:54:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ var normalizeUrl = require('normalize-url')
 
 var regUrl = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
 
-function validateUrl (val) {
+function validateUrl (val, options) {
+  var required = (typeof options.required === 'function') ? options.required() : options.required
+  var passedAllowBlank = options.allowBlank && (val === '' || val === null)
+  if (passedAllowBlank && !required) {
+    return true
+  }
   return regUrl.test(val)
 }
 
 function Url (path, options) {
+  this.options = options
+  this.path = path
   mongoose.SchemaTypes.String.call(this, path, options)
-  this.validate(validateUrl, 'url is invalid')
+  this.validate(function (val) { return validateUrl(val, options) }, options.message || Url.defaults.message || 'url is invalid')
 }
+
+Url.defaults = {}
 
 Object.setPrototypeOf(Url.prototype, mongoose.SchemaTypes.String.prototype)
 
 Url.prototype.cast = function (val) {
   return normalizeUrl(val)
+}
+
+Url.prototype.get = function (val) {
+  return normalizeUrl(val)
+}
+
+Url.prototype.checkRequired = function (val) {
+  return typeof val === 'string' && validateUrl(val, this.options)
 }
 
 mongoose.SchemaTypes.Url = module.exports = Url
